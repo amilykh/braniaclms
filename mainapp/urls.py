c6e2ecebcdebde35c6b189dc1299b1688173a6af
Andrey Milykh <amilykh@mail.ru>
2022-10-21 01:17:08 +0300

    correct loading of static files

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,10 +7,10 @@ app_name = MainappConfig.name
 
 
 urlpatterns = [
+    path('', views.IndexView.as_view(), name='home'),
     path('contacts/', views.ContactsView.as_view(), name='contacts'),
     path('courses/', views.CoursesListView.as_view(), name='courses'),
     path('docsite/', views.DocSiteView.as_view(), name='docsite'),
-    path('', views.IndexView.as_view(), name='index'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('news/', views.NewsView.as_view(), name='news'),
 ]
