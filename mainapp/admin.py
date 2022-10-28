45bc05849b850eb27b799a2b77e4894ab5a0a80d
Andrey Milykh <amilykh@mail.ru>
2022-10-28 06:28:04 +0300

    dz_4

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from mainapp.models import News
+
 # Register your models here.
+admin.site.register(News)
+
+
---
+++ mainapp/urls.py
@@ -0,0 +1,17 @@
+from mainapp import views
+from django.urls import path
+from mainapp.apps import MainappConfig
+
+
+app_name = MainappConfig.name
+
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('contacts/', views.ContactsView.as_view(), name='contacts'),
+    path('courses/', views.CoursesListView.as_view(), name='courses'),
+    path('docsite/', views.DocSiteView.as_view(), name='docsite'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('news/', views.NewsView.as_view(), name='news'),
+    path('news/<pk>/', views.NewsDetail.as_view(), name='news_detail'),
+]
